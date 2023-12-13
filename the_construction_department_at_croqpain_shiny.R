da39aa70f2b77423490d82a09c2aac45466859da
rsm-sal072 <sal072@ucsd.edu>
2023-12-13 01:30:51 -0800

    12122023

---
+++ the_construction_department_at_croqpain_shiny.R
@@ -0,0 +1,77 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(rmarkdown)
+library(shinyjs)
+
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      .navbar .navbar-nav {
+        width: 100%;
+      }
+
+      .navbar-brand {
+        font-size: 25px; 
+        margin-bottom: 30px;
+        font-family: 'Verdana', sans-serif; 
+      }
+    "))
+  ),
+  
+  navbarPage(
+    id = "main_nav",
+    title = div(class = "navbar-brand", "The Construction Department at Croq'Pain"),
+    tabPanel("📊",
+               fluidPage(
+                 pickerInput("chapter", "Choose an option:",
+                                      choices = c("Case Summary", 
+                                                  "Analysis")),
+                 uiOutput("chapter")))
+
+    
+    
+  )
+)
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+
+
+server <- function(input, output, session) {
+  active_tab <- reactiveVal("chapter")
+  
+  
+  ################################## Chapter 1: Descision Analysis ###########################################
+  
+  
+  output$chapter <- renderUI({
+    req(input$chapter) 
+    
+    file_name <- switch(input$chapter,
+                        "Case Summary" = "summary_the_construction_department_at_croqpain.html",
+                        "Analysis" = "analysis_the_construction_department_at_croqpain.html")
+    
+    if (!is.null(file_name)) {
+      tags$iframe(src = file_name, style = "width:100%; height:800px;")
+    }
+  })
+  
+  observeEvent(input$main_nav, {
+    active_tab(input$main_nav)
+  })
+  
+  
+  #########################################################################################################################################################
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+
